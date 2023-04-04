41d0450ee1a013aedd992fd770d3be8dea301f19
theluposki <theluposki@gmail.com>
2023-04-04 06:56:08 -0300

    exported and import

--- src/public/app.js
+++ src/public/app.js
@@ -1,28 +1,61 @@
-import { generateKey, encrypt, decrypt, pack, unpack } from "./cryptoUtil.js"
+import { generateKey, encrypt, decrypt } from "./cryptoUtil.js"
 
 const panelView = document.querySelector(".panelView")
 const text = document.getElementById("text")
+panelView.innerHTML = "Saida"
+const generatedKeyText = document.getElementById("generatedKey")
 
 let textEnc;
 
 async function init() {
-  const key = await generateKey()
 
-  const exported = await window.crypto.subtle.exportKey("jwk", key)
+  let CryptoKey;
 
-  console.log(key)
-  console.log(exported)
+  const genKey = async () => {
+    const key = await generateKey()
+    const exported = await window.crypto.subtle.exportKey("jwk", key)
+    
+    CryptoKey = key
+
+    localStorage.setItem("key", btoa(JSON.stringify(exported)))
+    generatedKeyText.value = btoa(JSON.stringify(exported)) 
+  }
+
+  
+  const getCurrentKey = async () => {
+    if(localStorage.getItem("key")) {
+      const currentKey = JSON.parse(atob(localStorage.getItem("key")))
+      return await window.crypto.subtle.importKey("jwk", currentKey, "AES-GCM", true, ["encrypt", "decrypt"]);
+    }
+    return await genKey()
+  }
+
+  CryptoKey = await getCurrentKey();
+
+  console.log(await getCurrentKey())
+
+  const impKey = async () => {
+    const importedKeyText = document.getElementById("importedKey")
+
+    localStorage.setItem("key", importedKeyText)
+    
+    const newKey = JSON.parse(atob(importedKeyText))
+    const key = await window.crypto.subtle.importKey("jwk", newKey, "AES-GCM", true, ["encrypt", "decrypt"]);
+    CryptoKey = key
+  }
+
+  document.getElementById("btnGeneratedKey").addEventListener("click", () => {
+    genKey()
+  })
+
+  document.getElementById("btnImportedKey").addEventListener("click", () => {
+    impKey()
+  })
 
   //localStorage.setItem("key", JSON.stringify(exported))
 
-  const imported = await window.crypto.subtle.importKey("jwk", JSON.parse(localStorage.getItem("key")), "AES-GCM", true, ["encrypt", "decrypt"]);
-  console.log(imported)
-  
-
-
-  
   document.getElementById("enc").addEventListener("click", async () => {
-    const result = await encrypt(text.value, imported)
+    const result = await encrypt(text.value, await CryptoKey)
 
     textEnc = result
 
@@ -32,7 +65,7 @@ async function init() {
 
   document.getElementById("dec").addEventListener("click", async () => {
     console.log(textEnc)
-    const result = await decrypt("eyJjaXBoZXIiOiJFYmRNRC9vN1F1TkVETkxZcmdOUGdzTzZSZDdWIiwiaXYiOiIrY0FJSXhvcmpCRnpScDlPIn0=", imported)
+    const result = await decrypt(textEnc, await CryptoKey)
 
     console.log(result)
     panelView.innerHTML = `<pre>${result}</pre>`
